e4626cb78252efae467d665b192974aef156ab09
An Tran <tranquocan012017@gmail.com>
2026-02-25 22:11:39 +0700

    feat: login

---
+++ src/main/java/com/pro/task_management/dto/request/AuthRequestDTO.java
@@ -0,0 +1,14 @@
+package com.pro.task_management.dto.request;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class AuthRequestDTO {
+    String username;
+    String password;
+}
